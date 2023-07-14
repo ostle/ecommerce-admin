d083effeb1efb9b5a1ea84f8a6e63c44b0b09bca
ostle <leoostrower@gmail.com>
2023-07-14 13:24:15 -0300

    setting-form done

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -14,6 +14,11 @@ export function MainNav({
 
 	const routes = [
 		{
+			href: `/${params.storeId}`,
+			label: 'Overview',
+			active: pathname === `/${params.storeId}`,
+		},
+		{
 			href: `/${params.storeId}/settings`,
 			label: 'Settings',
 			active: pathname === `/${params.storeId}/settings`,
@@ -21,7 +26,12 @@ export function MainNav({
 	];
 
 	return (
-		<nav className={cn('flex items-center space-x-4 lg:space-x-6', className)}>
+		<nav
+			className={cn(
+				'flex items-center space-x-4 lg:space-x-6',
+				className
+			)}
+		>
 			{routes.map((route) => (
 				<Link
 					key={route.href}
